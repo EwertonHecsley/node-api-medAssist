bf91b41a2c996f93205c9788891d4d3635f2e7e5
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2025-06-17 09:00:09 -0300

    feat: useCase buscar um paciente

--- src/core/domain/pacient/repository/PacientRepository.ts
+++ src/core/domain/pacient/repository/PacientRepository.ts
@@ -4,4 +4,6 @@ export abstract class PacientRepository {
   abstract create(entity: Pacient): Promise<Pacient>;
   abstract findByCPF(cpf: string): Promise<Pacient>;
   abstract findByEmail(email: string): Promise<Pacient>;
+  abstract findById(id: string): Promise<Pacient>;
+  abstract findAll(): Promise<Pacient[]>;
 }
